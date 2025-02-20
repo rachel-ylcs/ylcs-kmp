0650592b7c3d2d87655a4736b08ea6351b3bc7ae
masterQian <292869319@qq.com>
2025-02-20 11:01:58 +0800

    服务端增加本地配置

---
+++ server/src/main/kotlin/love/yinlin/Config.kt
@@ -0,0 +1,26 @@
+package love.yinlin
+
+import java.util.Properties
+
+object Config {
+	private val prop = Properties().apply {
+		val classLoader = Config::class.java.classLoader
+		val stream = classLoader.getResourceAsStream("config.properties")!!
+		load(stream)
+		stream.close()
+	}
+
+	val HOST: String = prop["host"] as String
+
+	object Mysql {
+		val PORT: Int = (prop["mysql.port"] as String).toInt()
+		val NAME: String = prop["mysql.name"] as String
+		val USERNAME: String = prop["mysql.username"] as String
+		val PASSWORD: String = prop["mysql.password"] as String
+	}
+
+	object Redis {
+		val PORT: Int = (prop["redis.port"] as String).toInt()
+		val PASSWORD: String = prop["redis.password"] as String
+	}
+}
