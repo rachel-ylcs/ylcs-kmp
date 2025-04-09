f5fc090a9cb6afe1fd00397aaae903d2b23a9101
masterQian <76944654+masterQian@users.noreply.github.com>
2025-04-09 20:56:58 +0800

    更新MOD协议文档

--- music/build.gradle.kts
+++ music/build.gradle.kts
@@ -26,6 +26,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(projects.shared)
+            implementation(libs.kotlinx.io)
         }
     }
 }
---
+++ music/src/commonMain/kotlin/love/yinlin/music/MusicFactory.kt
@@ -0,0 +1,5 @@
+package love.yinlin.music
+
+object MusicFactory {
+
+}
