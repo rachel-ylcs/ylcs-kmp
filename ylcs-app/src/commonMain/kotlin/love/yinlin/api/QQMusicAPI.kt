8cf2f08d0a869d00503c17e9e3286f5bc102149c
masterQian <76944654+masterQian@users.noreply.github.com>
2025-05-07 01:42:49 +0800

    refactor: 更新平台音乐框架

--- ylcs-app/src/commonMain/kotlin/love/yinlin/api/QQMusicAPI.kt
+++ ylcs-app/src/commonMain/kotlin/love/yinlin/api/QQMusicAPI.kt
@@ -1,5 +1,6 @@
 package love.yinlin.api
 
+import kotlinx.serialization.json.JsonObject
 import love.yinlin.common.Uri
 import love.yinlin.data.Data
 import love.yinlin.data.music.PlatformMusicInfo
@@ -13,37 +14,48 @@ import love.yinlin.platform.app
 import love.yinlin.platform.safeGet
 
 object QQMusicAPI {
-    private inline fun buildUrl(url: String, data: JsonObjectScope.() -> Unit): String = "$url?data=${Uri.encodeUri(makeObject(data).toJsonString())}"
+    private inline fun buildUrl(data: JsonObjectScope.() -> Unit): String =
+        "https://u.y.qq.com/cgi-bin/musicu.fcg?data=${Uri.encodeUri(makeObject(data).toJsonString())}"
 
-    private fun decodeData(body: ByteArray) = body.decodeToString().parseJson.Object.obj("req_0").obj("data")
+    private fun decodeData(num: Int, body: ByteArray): List<JsonObject> {
+        val json = body.decodeToString().parseJson.Object
+        val arr = mutableListOf<JsonObject>()
+        repeat(num) {
+            arr += json.obj("req_$it").obj("data")
+        }
+        return arr
+    }
 
     suspend fun requestMusicId(url: String): Data<String> = app.client.safeGet(url) { body: ByteArray ->
         "\"mid\":\\s*\"([^\"]*)".toRegex().find(body.decodeToString())!!.groupValues[1]
     }
 
     suspend fun requestMusic(id: String): Data<PlatformMusicInfo> {
-        app.client.safeGet(buildUrl("https://u.y.qq.com/cgi-bin/musicu.fcg") {
+        app.client.safeGet(buildUrl {
             obj("req_0") {
+                "module" with "music.pf_song_detail_svr"
+                "method" with "get_song_detail_yqq"
+                obj("param") { "song_mid" with id }
+            }
+            obj("req_1") {
+                "module" with "music.musichallSong.PlayLyricInfo"
+                "method" with "GetPlayLyricInfo"
+                obj("param") {
+                    "songMID" with id
+                }
+            }
+            obj("req_2") {
                 "module" with "vkey.GetVkeyServer"
                 "method" with "CgiGetVkey"
                 obj("param") {
-                    "guid" with "10000"
-                    "uin" to "0"
-                    "loginflag" to 1
-                    "platform" to "20"
-                    arr("filename") { add("C400${id}.m4a") }
+                    arr("filename") { add("C400$id$id.m4a") }
                     arr("songmid") { add(id) }
                     arr("songtype") { add(0) }
+                    "guid" to "10000"
                 }
             }
-            obj("comm") {
-                "uin" with "0"
-                "format" with "json"
-                "ct" with 24
-                "cv" with 0
-            }
         }) { body: ByteArray ->
-            val json = decodeData(body)
+            val json = decodeData(3, body)
             println(json)
         }
         return Data.Error()
