161cc8ef182466833561fbe693082dc4111c58d3
masterQian <76944654+masterQian@users.noreply.github.com>
2025-05-08 17:16:55 +0800

    Update Theme.kt

--- ylcs-app/src/commonMain/kotlin/love/yinlin/common/Theme.kt
+++ ylcs-app/src/commonMain/kotlin/love/yinlin/common/Theme.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import love.yinlin.extension.localComposition
 import org.jetbrains.compose.resources.Font
 import love.yinlin.resources.Res
 import love.yinlin.resources.xwwk
