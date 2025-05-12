fe9fb5291b2ee149991e691babf5128593432876
masterQian <76944654+masterQian@users.noreply.github.com>
2025-05-12 13:01:45 +0800

    Update Theme.kt

--- ylcs-app/src/commonMain/kotlin/love/yinlin/common/Theme.kt
+++ ylcs-app/src/commonMain/kotlin/love/yinlin/common/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
@@ -203,7 +204,7 @@ private fun baseTextStyle(
 	size: TextUnit,
 	isBold: Boolean = false
 ): TextStyle = TextStyle(
-	fontFamily = font,
+	fontFamily = FontFamily(font),
 	fontWeight = if (isBold) FontWeight.SemiBold else FontWeight.Light,
 	fontSize = size,
 	lineHeight = size * 1.5f,
