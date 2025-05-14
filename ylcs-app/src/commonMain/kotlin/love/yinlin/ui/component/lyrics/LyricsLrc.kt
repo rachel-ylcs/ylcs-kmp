5ead4159f762b1a985a703b2d545142db6c2ba50
masterQian <76944654+masterQian@users.noreply.github.com>
2025-05-14 15:55:44 +0800

    Update LyricsLrc.kt

--- ylcs-app/src/commonMain/kotlin/love/yinlin/ui/component/lyrics/LyricsLrc.kt
+++ ylcs-app/src/commonMain/kotlin/love/yinlin/ui/component/lyrics/LyricsLrc.kt
@@ -27,7 +27,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.DpRect
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import kotlinx.coroutines.flow.map
 import kotlinx.serialization.Serializable
