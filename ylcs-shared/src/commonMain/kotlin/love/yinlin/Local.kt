7ba47aa6c5b189036e972ccdcbc3a7321d92de6d
masterQian <76944654+masterQian@users.noreply.github.com>
2025-05-16 11:43:49 +0800

    Update Local.kt

--- ylcs-shared/src/commonMain/kotlin/love/yinlin/Local.kt
+++ ylcs-shared/src/commonMain/kotlin/love/yinlin/Local.kt
@@ -4,8 +4,8 @@ package love.yinlin
 object Local {
 	const val APP_NAME: String = "ylcs"
 	const val NAME: String = "银临茶舍"
-	const val VERSION: Int = 300
-	const val VERSION_NAME: String = "3.0.0"
+	const val VERSION: Int = 301
+	const val VERSION_NAME: String = "3.0.1"
 
 	// 客户端配置
 	object Client {
