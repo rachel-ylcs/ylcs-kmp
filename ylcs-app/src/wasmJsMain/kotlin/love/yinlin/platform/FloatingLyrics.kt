6bdc1bc7a4fc33d51fe969a1f8b7bfa9635bfb73
masterQian <76944654+masterQian@users.noreply.github.com>
2025-05-22 15:41:05 +0800

    Update FloatingLyrics.kt

--- ylcs-app/src/wasmJsMain/kotlin/love/yinlin/platform/FloatingLyrics.kt
+++ ylcs-app/src/wasmJsMain/kotlin/love/yinlin/platform/FloatingLyrics.kt
@@ -1,25 +1,18 @@
 package love.yinlin.platform
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 
 @Stable
 class ActualFloatingLyrics : FloatingLyrics() {
-    override val canAttached: Boolean = false
     override val isAttached: Boolean = false
 
-    override fun applyPermission(onResult: (Boolean) -> Unit) {
-
-    }
-
-    override fun attach() {
-
-    }
-
-    override fun detach() {
-
-    }
-
     override fun updateLyrics(lyrics: String?) {
+
+    }
+
+    @Composable
+    override fun Content() {
 
     }
 }
