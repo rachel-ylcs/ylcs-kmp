72f8f0ad0e45bc1a50b1fe6d0a0152bac42eb1f2
正版青尘 <201436728@qq.com>
2025-06-01 04:20:50 +0800

    feat(desktop): 实现macos/linux单实例运行 close #66

--- native/platform/linux/message.cpp
+++ native/platform/linux/message.cpp
@@ -1,8 +1,39 @@
 #include "platform.h"
 
+#include <string>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/file.h>
+
+#define LOCK_FILE_PATH "/tmp/ylcs_desktop.lock"
+
+static int appLockFd = -1;
+
 bool ylcs_single_instance_try_lock() {
-    return false;
+    appLockFd = open(LOCK_FILE_PATH, O_CREAT | O_RDWR, 0666);
+    if (appLockFd < 0) {
+        return false;
+    }
+
+    int result = flock(appLockFd, LOCK_EX | LOCK_NB);
+    if (result < 0) {
+        close(appLockFd);
+        appLockFd = -1;
+        return false;
+    }
+
+    std::string pid = std::to_string(getpid());
+    ftruncate(appLockFd, 0);
+    write(appLockFd, pid.c_str(), pid.length());
+    return true;
 }
 
 void ylcs_single_instance_unlock() {
+    if (appLockFd >= 0) {
+        flock(appLockFd, LOCK_UN);
+        close(appLockFd);
+        appLockFd = -1;
+        unlink(LOCK_FILE_PATH);
+    }
 }
--- native/platform/macos/message.cpp
+++ native/platform/macos/message.cpp
@@ -1,8 +1,26 @@
 #include "platform.h"
 
+#include "CoreFoundation/CoreFoundation.h"
+
+static CFMessagePortRef appMessagePort = NULL;
+
+static CFDataRef MessageCallback(CFMessagePortRef port, SInt32 msgid, CFDataRef data, void *info) {
+    return NULL;
+}
+
 bool ylcs_single_instance_try_lock() {
+    CFStringRef name = CFSTR("love.yinlin.ylcs-desktop");
+    appMessagePort = CFMessagePortCreateLocal(nil, name, MessageCallback, nil, nil);
+    if (appMessagePort != NULL) {
+        return true;
+    }
     return false;
 }
 
 void ylcs_single_instance_unlock() {
+    if (appMessagePort != NULL) {
+        CFMessagePortInvalidate(appMessagePort);
+        CFRelease(appMessagePort);
+        appMessagePort = NULL;
+    }
 }
