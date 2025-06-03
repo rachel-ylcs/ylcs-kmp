589d93d1ac9839da4f6001816dd1fd8f7dcab079
masterQian <76944654+masterQian@users.noreply.github.com>
2025-06-03 22:39:29 +0800

    Update PAGAnimation.kt

--- ylcs-app/src/commonMain/kotlin/love/yinlin/ui/component/platform/PAGAnimation.kt
+++ ylcs-app/src/commonMain/kotlin/love/yinlin/ui/component/platform/PAGAnimation.kt
@@ -12,6 +12,13 @@ object PAGConfig {
     const val INFINITY = -1
 }
 
+// 简单动画
+// data: 动画源
+// repeatCount: 重复次数
+// renderScale: 缩放比例
+// scaleMode: 缩放模式
+// cacheAllFramesInMemory: 启用内存缓存
+
 @Composable
 expect fun PAGImageAnimation(
     data: ByteArray,
@@ -27,6 +34,10 @@ expect class PAGState() {
     var data: ByteArray
     var progress: Double
 }
+
+// 帧动画
+// 通过PAGState控制动画源data和当前进度progress(0 ~ 1)
+// 动画初始状态不播放, 在协程中变化进度来控制播放
 
 @Composable
 expect fun PAGAnimation(
