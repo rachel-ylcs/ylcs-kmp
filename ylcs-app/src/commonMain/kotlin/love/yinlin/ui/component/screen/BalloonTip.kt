42be3ced547a44acebac2cc6c87fbc79562fbaf8
masterQian <76944654+masterQian@users.noreply.github.com>
2025-06-22 15:55:28 +0800

    feat: 气球提示框支持

---
+++ ylcs-app/src/commonMain/kotlin/love/yinlin/ui/component/screen/BalloonTip.kt
@@ -0,0 +1,77 @@
+package love.yinlin.ui.component.screen
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PlainTooltip
+import androidx.compose.material3.RichTooltip
+import androidx.compose.material3.RichTooltipColors
+import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
+import androidx.compose.runtime.Composable
+import love.yinlin.common.ThemeValue
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BallonTip(
+    text: String,
+    content: @Composable () -> Unit
+) {
+    TooltipBox(
+        positionProvider = TooltipDefaults.rememberTooltipPositionProvider(),
+        tooltip = {
+            PlainTooltip(
+                contentColor = MaterialTheme.colorScheme.onBackground,
+                containerColor = MaterialTheme.colorScheme.background,
+                tonalElevation = ThemeValue.Shadow.Tonal,
+                shadowElevation = ThemeValue.Shadow.MiniSurface
+            ) {
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.labelMedium
+                )
+            }
+        },
+        state = rememberTooltipState(),
+        content = content
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BallonTip(
+    title: String,
+    message: String,
+    content: @Composable () -> Unit
+) {
+    TooltipBox(
+        positionProvider = TooltipDefaults.rememberTooltipPositionProvider(),
+        tooltip = {
+            RichTooltip(
+                title = {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                },
+                text = {
+                    Text(
+                        text = message,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                },
+                colors = RichTooltipColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                    titleContentColor = MaterialTheme.colorScheme.onBackground,
+                    actionContentColor = MaterialTheme.colorScheme.primary
+                ),
+                tonalElevation = ThemeValue.Shadow.Tonal,
+                shadowElevation = ThemeValue.Shadow.MiniSurface
+            )
+        },
+        state = rememberTooltipState(),
+        content = content
+    )
+}
--- ylcs-app/src/commonMain/kotlin/love/yinlin/ui/screen/msg/pictures/ScreenPictures.kt
+++ ylcs-app/src/commonMain/kotlin/love/yinlin/ui/screen/msg/pictures/ScreenPictures.kt
@@ -12,14 +12,10 @@ import androidx.compose.material.icons.outlined.Refresh
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TooltipBox
-import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
 import androidx.compose.material3.carousel.rememberCarouselState
-import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
