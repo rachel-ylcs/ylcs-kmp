92e06a773d8a4c76fcdb1cea7b47dff74871c0ea
masterQian <76944654+masterQian@users.noreply.github.com>
2025-07-04 00:05:28 +0800

    feat: 屏幕旋转支持

--- ylcs-app/src/wasmJsMain/kotlin/love/yinlin/ui/component/platform/OrientationController.kt
+++ ylcs-app/src/wasmJsMain/kotlin/love/yinlin/ui/component/platform/OrientationController.kt
@@ -1,2 +1,12 @@
 package love.yinlin.ui.component.platform
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+
+@Composable
+actual fun rememberOrientationController(): OrientationController {
+    return remember { object : OrientationController {
+        override var orientation: Orientation = Orientation.LANDSCAPE
+        override fun rotate() {}
+    } }
+}
