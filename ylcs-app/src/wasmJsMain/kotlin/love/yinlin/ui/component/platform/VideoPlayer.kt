baf43e8ba6db911779a53d42aac6fd37a62e4b69
masterQian <76944654+masterQian@users.noreply.github.com>
2025-07-05 03:15:07 +0800

    feat: Web端支持视频播放

--- ylcs-app/src/wasmJsMain/kotlin/love/yinlin/ui/component/platform/VideoPlayer.kt
+++ ylcs-app/src/wasmJsMain/kotlin/love/yinlin/ui/component/platform/VideoPlayer.kt
@@ -1,8 +1,15 @@
 package love.yinlin.ui.component.platform
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import love.yinlin.platform.UnsupportedComponent
+import kotlinx.browser.document
+import love.yinlin.data.MimeType
+import love.yinlin.extension.OffScreenEffect
+import love.yinlin.ui.CustomUI
+import org.w3c.dom.HTMLSourceElement
+import org.w3c.dom.HTMLVideoElement
 
 @Composable
 actual fun VideoPlayer(
@@ -10,5 +17,32 @@ actual fun VideoPlayer(
     modifier: Modifier,
     onBack: () -> Unit
 ) {
-    UnsupportedComponent(modifier = modifier)
+    val view = remember { mutableStateOf<HTMLVideoElement?>(null) }
+
+    OffScreenEffect { isForeground ->
+        view.value?.let { video ->
+            if (isForeground) video.play()
+            else video.pause()
+        }
+    }
+
+    CustomUI(
+        view = view,
+        factory = {
+            (document.createElement("video") as HTMLVideoElement).also { video ->
+                video.autoplay = true
+                video.controls = true
+                video.loop = true
+                video.muted = false
+                (document.createElement("source") as HTMLSourceElement).also { source ->
+                    source.type = MimeType.MP4
+                    video.appendChild(source)
+                }
+            }
+        },
+        update = { view ->
+            (view.firstElementChild as? HTMLSourceElement)?.src = url
+        },
+        modifier = modifier,
+    )
 }
