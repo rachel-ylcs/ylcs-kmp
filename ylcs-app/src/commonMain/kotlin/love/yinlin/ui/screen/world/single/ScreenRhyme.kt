e5745f28229c5f70fbf0c4e7e12fcdeeac466cbf
masterQian <76944654+masterQian@users.noreply.github.com>
2025-07-11 19:00:21 +0800

    feat: 通用平台游戏页框架设计

--- ylcs-app/src/commonMain/kotlin/love/yinlin/ui/screen/world/single/ScreenRhyme.kt
+++ ylcs-app/src/commonMain/kotlin/love/yinlin/ui/screen/world/single/ScreenRhyme.kt
@@ -3,21 +3,17 @@ package love.yinlin.ui.screen.world.single
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.zIndex
+import kotlinx.coroutines.delay
 import love.yinlin.AppModel
-import love.yinlin.common.Colors
 import love.yinlin.common.Device
-import love.yinlin.extension.rememberDerivedState
+import love.yinlin.ui.component.layout.LoadingBox
 import love.yinlin.ui.component.node.clickableNoRipple
 import love.yinlin.ui.component.screen.CommonSubScreen
 
@@ -25,83 +21,137 @@ import love.yinlin.ui.component.screen.CommonSubScreen
 private enum class GameState {
     Loading, // 加载中
     Start, // 开始
-    PortraitLock, // 竖屏锁
-    Pause, // 暂停
-    Resume, // 恢复准备
     Playing, // 游戏中
     Settling, // 结算
 }
 
 @Stable
-class ScreenRhyme(model: AppModel) : CommonSubScreen(model) {
-    override val title: String? = null
+private sealed interface GameLockState {
+    @Stable
+    data object Normal : GameLockState // 正常
+    @Stable
+    data object PortraitLock : GameLockState // 竖屏锁
+    @Stable
+    data object Pause : GameLockState // 暂停
+    @Stable
+    data class Resume(val time: Int) : GameLockState // 恢复准备
+}
 
-    private var state by mutableStateOf(GameState.Loading)
+@Stable
+class ScreenRhyme(model: AppModel) : CommonSubScreen(model) {
+    private var state: GameState by mutableStateOf(GameState.Loading)
+    private var lockState: GameLockState by mutableStateOf(GameLockState.Normal)
+
+    private fun pauseGame() {
+
+    }
 
     private fun resumePauseTimer() {
-        state = GameState.Resume
+        lockState = GameLockState.Resume(3)
     }
 
     @Composable
-    private fun Stage(modifier: Modifier, constraints: Constraints) {
-        Canvas(modifier = modifier) {
-            drawRect(Colors.Black)
+    private fun GameOverlayLoading() {
+        Box(modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background)) {
+            LoadingBox()
         }
+    }
+
+    @Composable
+    private fun GameOverlayStart() {
+
+    }
+
+    @Composable
+    private fun GameOverlayPlaying() {
+
+    }
+
+    @Composable
+    private fun GameOverlaySettling() {
+
+    }
+
+    @Composable
+    private fun GameOverlay(modifier: Modifier) {
+        Box(modifier = modifier) {
+            when (state) {
+                GameState.Loading -> GameOverlayLoading()
+                GameState.Start -> GameOverlayStart()
+                GameState.Playing -> GameOverlayPlaying()
+                GameState.Settling -> GameOverlaySettling()
+            }
+        }
+    }
+
+    @Composable
+    private fun GameScrimMask(modifier: Modifier) {
+        Box(modifier = modifier) {
+            if (lockState !is GameLockState.Normal) {
+                Box(modifier = modifier
+                    .background(MaterialTheme.colorScheme.scrim.copy(alpha = 0.4f))
+                    .clickableNoRipple { }
+                )
+            }
+        }
+    }
+
+    @Composable
+    private fun GameStage(modifier: Modifier) {
+        Canvas(modifier = modifier) {
+            val scale = 1920 / size.width
+        }
+    }
+
+    @Composable
+    private fun GameBackground(modifier: Modifier) {
+        Box(modifier = modifier)
+    }
+
+    override val title: String? = null
+
+    override suspend fun initialize() {
+        // 加载游戏
+        delay(3000L)
+        state = GameState.Start
     }
 
     @Composable
     override fun SubContent(device: Device) {
         LaunchedEffect(device.type) {
             if (device.type == Device.Type.LANDSCAPE) {
-                if (state == GameState.PortraitLock) resumePauseTimer()
+                if (lockState is GameLockState.PortraitLock) resumePauseTimer()
             }
-            else state = GameState.PortraitLock
+            else {
+                lockState = GameLockState.PortraitLock
+                pauseGame()
+            }
         }
-        BoxWithConstraints(
+
+        Layout(
             modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            // 状态层
-            when (state) {
-                GameState.Loading -> {
-
-                }
-                GameState.Start -> {
-
-                }
-                GameState.PortraitLock -> {
-
-                }
-                GameState.Pause -> {
-
-                }
-                GameState.Resume -> {
-
-                }
-                GameState.Playing -> {
-
-                }
-                GameState.Settling -> {
-
+            content = {
+                // 状态层
+                GameOverlay(modifier = Modifier.fillMaxSize().zIndex(4f))
+                // 遮罩层
+                GameScrimMask(modifier = Modifier.fillMaxSize().zIndex(3f))
+                // 画布层
+                GameStage(modifier = Modifier.fillMaxSize().zIndex(2f))
+                // 背景层
+                GameBackground(modifier = Modifier.fillMaxSize().zIndex(1f))
+            },
+            measurePolicy = { measurables, constraints ->
+                val maxWidth = constraints.maxWidth
+                val maxHeight = constraints.maxHeight
+                val useWidthFirst = maxWidth * 9 <= maxHeight * 16
+                val childWidth = if (useWidthFirst) maxWidth else maxHeight * 16 / 9
+                val childHeight = if (useWidthFirst) maxWidth * 9 / 16 else maxHeight
+                val childConstraints = Constraints.fixed(childWidth, childHeight)
+                val placeables = measurables.map { it.measure(childConstraints) }
+                layout(childWidth, childHeight) {
+                    placeables.forEach { it.placeRelative(0, 0) }
                 }
             }
-            // 遮罩层
-            if (state != GameState.Playing) {
-                Box(modifier = Modifier.fillMaxSize()
-                    .background(MaterialTheme.colorScheme.scrim.copy(alpha = 0.4f))
-                    .clickableNoRipple { }
-                    .zIndex(3f)
-                )
-            }
-            // 游戏画布层
-            val widthFirstly by rememberDerivedState { constraints.maxWidth * 9 <= constraints.maxHeight * 16 }
-            Stage(
-                modifier = Modifier.then(if (widthFirstly) Modifier.fillMaxWidth() else Modifier.fillMaxHeight())
-                    .aspectRatio(16 / 9f).zIndex(2f),
-                constraints = constraints
-            )
-            // 背景层
-            Box(modifier = Modifier.fillMaxSize().zIndex(1f))
-        }
+        )
     }
 }
