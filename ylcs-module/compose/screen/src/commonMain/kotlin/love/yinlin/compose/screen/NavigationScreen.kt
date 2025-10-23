cfa7992fb166682af844481dd707adff0d28b4d9
masterQian <76944654+masterQian@users.noreply.github.com>
2025-10-24 00:45:19 +0800

    fix: 修复ScreenMain切换子页面动画异常的问题

--- ylcs-module/compose/screen/src/commonMain/kotlin/love/yinlin/compose/screen/NavigationScreen.kt
+++ ylcs-module/compose/screen/src/commonMain/kotlin/love/yinlin/compose/screen/NavigationScreen.kt
@@ -1,15 +1,18 @@
 package love.yinlin.compose.screen
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import love.yinlin.compose.Device
 import love.yinlin.compose.LaunchFlag
+import love.yinlin.compose.LocalAnimationSpeed
 import love.yinlin.compose.LocalDevice
-import love.yinlin.compose.ui.floating.FABAction
 import love.yinlin.compose.ui.floating.FABLayout
 import kotlin.reflect.KClass
 
@@ -37,8 +40,28 @@ abstract class NavigationScreen<A>(manager: ScreenManager) : BasicScreen<A>(mana
 
     @Composable
     override fun BasicContent() {
+        val animationSpeed = LocalAnimationSpeed.current
         Wrapper(LocalDevice.current, pageIndex) { device ->
-            AnimatedContent(targetState = pageIndex) { index ->
+            AnimatedContent(
+                targetState = pageIndex,
+                transitionSpec = {
+                    val enterAnimation = slideIntoContainer(
+                        towards = AnimatedContentTransitionScope.SlideDirection.Start,
+                        animationSpec = tween(
+                            durationMillis = animationSpeed,
+                            easing = FastOutSlowInEasing
+                        )
+                    )
+                    val exitAnimation = slideOutOfContainer(
+                        towards = AnimatedContentTransitionScope.SlideDirection.End,
+                        animationSpec = tween(
+                            durationMillis = animationSpeed,
+                            easing = FastOutSlowInEasing
+                        )
+                    )
+                    enterAnimation.togetherWith(exitAnimation)
+                }
+            ) { index ->
                 Box(modifier = Modifier.fillMaxSize()) {
                     val screen = subs[index].screen
                     screen.Content(device)
