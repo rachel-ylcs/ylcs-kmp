6f39731769cb94acc802bb816c2533072b37856a
masterQian <76944654+masterQian@users.noreply.github.com>
2025-10-28 23:00:57 +0800

    fix: 修复context

--- ylcs-app/app/src/androidMain/kotlin/love/yinlin/screen/music/loader/ScreenImportMusic.android.kt
+++ ylcs-app/app/src/androidMain/kotlin/love/yinlin/screen/music/loader/ScreenImportMusic.android.kt
@@ -1,6 +1,7 @@
 package love.yinlin.screen.music.loader
 
+import love.yinlin.service
 import love.yinlin.uri.ContentUri
 import love.yinlin.uri.ImplicitUri
 
-actual fun processImportMusicDeepLink(deepLink: String): ImplicitUri = ContentUri(deepLink)
+actual fun processImportMusicDeepLink(deepLink: String): ImplicitUri = ContentUri(service.context.platformContext, deepLink)
