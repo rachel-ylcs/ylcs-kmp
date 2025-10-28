d02fe3af8bd8c8139e36a9c49f94a41e1559a2d1
masterQian <76944654+masterQian@users.noreply.github.com>
2025-10-28 23:25:43 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,9 +14,9 @@ dependencyResolutionManagement {
         maven("https://maven.aliyun.com/repository/central")
         maven("https://maven.aliyun.com/repository/public")
         maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         maven("https://jitpack.io")
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 
     versionCatalogs {
