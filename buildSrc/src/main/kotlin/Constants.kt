0c466e5a2f2a671fa546e62d9d3ccc249d984d1a
masterQian <76944654+masterQian@users.noreply.github.com>
2025-11-02 12:52:24 +0800

    update: 更新版本号: 3.3.0

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -2,6 +2,143 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
+class Constants(project: Project) {
+    // 环境
+    val environment = BuildEnvironment.Prod
+
+    // 平台
+    val platform = System.getProperty("os.name").lowercase().let {
+        when {
+            it.startsWith("windows") -> BuildPlatform.Windows
+            it.startsWith("mac") -> BuildPlatform.Mac
+            else -> BuildPlatform.Linux
+        }
+    }
+
+    // 平台版本
+    val platformVersion = System.getProperty("os.version") ?: ""
+
+    // 架构
+    val architecture = System.getProperty("os.arch").lowercase().let {
+        when {
+            it.startsWith("aarch64") -> BuildArchitecture.AARCH64
+            it.startsWith("arm") -> BuildArchitecture.ARM
+            // Windows x86_64
+            it.startsWith("amd64") -> BuildArchitecture.X86_64
+            // macOS Intel
+            it.startsWith("x86_64") -> BuildArchitecture.X86_64
+            else -> error("Unsupported architecture: $it")
+        }
+    }
+
+    // Gradle版本
+    val gradleVersion = project.gradle.gradleVersion
+
+    // Java版本
+    val javaVersion = System.getProperty("java.version") ?: ""
+
+    // 最大内存
+    val maxMemory = Runtime.getRuntime().maxMemory()
+
+    // 已分配内存
+    val totalMemory = Runtime.getRuntime().totalMemory()
+
+    // 可用内存
+    val freeMemory = Runtime.getRuntime().freeMemory()
+
+    // kotlin 特性
+    val features = listOf(
+        "-Xexpect-actual-classes",
+        "-Xnested-type-aliases",
+        "-Xwasm-kclass-fqn",
+        "-Xdata-flow-based-exhaustiveness",
+        "-Xes-long-as-bigint",
+    )
+
+    // 排除资源
+    val excludes = listOf(
+        "/META-INF/{AL2.0,LGPL2.1}",
+        "DebugProbesKt.bin"
+    )
+
+    // APP
+    val app = Application(
+        name = "ylcs",
+        projectName = "ylcs-app",
+        displayName = "银临茶舍",
+        version = 330,
+        versionName = "3.3.0",
+        description = "银临茶舍KMP跨平台APP",
+        vendor = "银临茶舍",
+        copyright = "© 2024-2025 银临茶舍 版权所有",
+        homepage = "https://github.com/rachel-ylcs/ylcs-kmp",
+        packageName = "love.yinlin"
+    )
+
+    // JVM 配置
+    val jvm = JvmConfig(
+        version = 25,
+        target = JvmTarget.JVM_25,
+        compatibility = JavaVersion.VERSION_25
+    )
+
+    // Android 配置
+    val android = AndroidConfig(
+        minSdk = 29,
+        compileSdk = 36,
+        targetSdk = 36,
+        ndkAbi = arrayOf("arm64-v8a"),
+        outputName = "ylcs.apk"
+    )
+
+    // iOS 配置
+    val ios = IosConfig(
+        target = "16.0"
+    )
+
+    // Desktop 配置
+    val desktop = DesktopConfig(
+        modules = arrayOf(
+            "java.instrument",
+            "java.net.http",
+            "java.management",
+            "jdk.unsupported",
+        )
+    )
+
+    // Server 配置
+    val server = ServerConfig(
+        outputName = "ylcs.jar"
+    )
+
+    // ModManager 配置
+    val modManager = ModManagerConfig(
+        name = "ModManager",
+        displayName = "银临茶舍Mod管理器",
+        mainClass = "${app.packageName}.ModManagerKt"
+    )
+
+    // 混淆配置
+    val proguard = ProguardConfig(
+        version = "7.8.1",
+        defaultRule = "proguard-android-optimize.txt"
+    )
+
+    // Host 配置
+    val host = HostConfig(
+        environment = environment,
+        mainHost = "yinlin.love",
+        mainPort = 1211,
+        webUseProxy = "webRun" in project.currentTaskName,
+        webServerPort = 8000
+    )
+
+    private val rootProject = project.rootProject
+
+    // 目录树
+    val root = RootProjectNode(rootProject.layout.projectDirectory, this)
+}
+
 enum class BuildEnvironment { Dev, Prod }
 
 enum class BuildPlatform {
@@ -89,143 +226,6 @@ class HostConfig(
         BuildEnvironment.Prod -> "https://$apiHost"
     }
     val webServerUrl: String = "http://localhost:$webServerPort"
-}
-
-class Constants(project: Project) {
-    private val rootProject = project.rootProject
-
-    // 环境
-    val environment = BuildEnvironment.Prod
-
-    // 平台
-    val platform = System.getProperty("os.name").lowercase().let {
-        when {
-            it.startsWith("windows") -> BuildPlatform.Windows
-            it.startsWith("mac") -> BuildPlatform.Mac
-            else -> BuildPlatform.Linux
-        }
-    }
-
-    // 平台版本
-    val platformVersion = System.getProperty("os.version") ?: ""
-
-    // 架构
-    val architecture = System.getProperty("os.arch").lowercase().let {
-        when {
-            it.startsWith("aarch64") -> BuildArchitecture.AARCH64
-            it.startsWith("arm") -> BuildArchitecture.ARM
-            // Windows x86_64
-            it.startsWith("amd64") -> BuildArchitecture.X86_64
-            // macOS Intel
-            it.startsWith("x86_64") -> BuildArchitecture.X86_64
-            else -> error("Unsupported architecture: $it")
-        }
-    }
-
-    // Gradle版本
-    val gradleVersion = project.gradle.gradleVersion
-
-    // Java版本
-    val javaVersion = System.getProperty("java.version") ?: ""
-
-    // 最大内存
-    val maxMemory = Runtime.getRuntime().maxMemory()
-
-    // 已分配内存
-    val totalMemory = Runtime.getRuntime().totalMemory()
-
-    // 可用内存
-    val freeMemory = Runtime.getRuntime().freeMemory()
-
-    // kotlin 特性
-    val features = listOf(
-        "-Xexpect-actual-classes",
-        "-Xnested-type-aliases",
-        "-Xwasm-kclass-fqn",
-        "-Xdata-flow-based-exhaustiveness",
-        "-Xes-long-as-bigint",
-    )
-
-    // 排除资源
-    val excludes = listOf(
-        "/META-INF/{AL2.0,LGPL2.1}",
-        "DebugProbesKt.bin"
-    )
-
-    // APP
-    val app = Application(
-        name = "ylcs",
-        projectName = "ylcs-app",
-        displayName = "银临茶舍",
-        version = 320,
-        versionName = "3.2.0",
-        description = "银临茶舍KMP跨平台APP",
-        vendor = "银临茶舍",
-        copyright = "© 2024-2025 银临茶舍 版权所有",
-        homepage = "https://github.com/rachel-ylcs/ylcs-kmp",
-        packageName = "love.yinlin"
-    )
-
-    // JVM 配置
-    val jvm = JvmConfig(
-        version = 25,
-        target = JvmTarget.JVM_25,
-        compatibility = JavaVersion.VERSION_25
-    )
-
-    // Android 配置
-    val android = AndroidConfig(
-        minSdk = 29,
-        compileSdk = 36,
-        targetSdk = 36,
-        ndkAbi = arrayOf("arm64-v8a"),
-        outputName = "ylcs.apk"
-    )
-
-    // iOS 配置
-    val ios = IosConfig(
-        target = "16.0"
-    )
-
-    // Desktop 配置
-    val desktop = DesktopConfig(
-        modules = arrayOf(
-            "java.instrument",
-            "java.net.http",
-            "java.management",
-            "jdk.unsupported",
-        )
-    )
-
-    // Server 配置
-    val server = ServerConfig(
-        outputName = "ylcs.jar"
-    )
-
-    // ModManager 配置
-    val modManager = ModManagerConfig(
-        name = "ModManager",
-        displayName = "银临茶舍Mod管理器",
-        mainClass = "${app.packageName}.ModManagerKt"
-    )
-
-    // 混淆配置
-    val proguard = ProguardConfig(
-        version = "7.8.1",
-        defaultRule = "proguard-android-optimize.txt"
-    )
-
-    // Host 配置
-    val host = HostConfig(
-        environment = environment,
-        mainHost = "yinlin.love",
-        mainPort = 1211,
-        webUseProxy = "webRun" in project.currentTaskName,
-        webServerPort = 8000
-    )
-
-    // 目录树
-    val root = RootProjectNode(rootProject.layout.projectDirectory, this)
 }
 
 val projectMap = mutableMapOf<Project, Constants>()
