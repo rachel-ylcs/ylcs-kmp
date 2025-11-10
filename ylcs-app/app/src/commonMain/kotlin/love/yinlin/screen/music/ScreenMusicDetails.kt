d996ee254c8573a8e10eda22998c841043b19b18
masterQian <76944654+masterQian@users.noreply.github.com>
2025-11-10 12:25:46 +0800

    refactor: 调整SongAPI结构

--- ylcs-app/app/src/commonMain/kotlin/love/yinlin/screen/music/ScreenMusicDetails.kt
+++ ylcs-app/app/src/commonMain/kotlin/love/yinlin/screen/music/ScreenMusicDetails.kt
@@ -1,15 +1,45 @@
 package love.yinlin.screen.music
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import love.yinlin.api.APIConfig
 import love.yinlin.app
+import love.yinlin.common.Paths
+import love.yinlin.compose.CustomTheme
 import love.yinlin.compose.Device
+import love.yinlin.compose.LocalImmersivePadding
 import love.yinlin.compose.screen.Screen
 import love.yinlin.compose.screen.ScreenManager
-import love.yinlin.data.music.Song
+import love.yinlin.compose.ui.image.PauseLoading
+import love.yinlin.compose.ui.layout.ActionScope
+import love.yinlin.compose.ui.layout.Pagination
+import love.yinlin.compose.ui.layout.PaginationColumn
+import love.yinlin.data.mod.ModResourceType
+import love.yinlin.data.rachel.song.Song
+import love.yinlin.data.rachel.song.SongComment
+import love.yinlin.extension.exists
 
 @Stable
-class ScreenMusicDetails(manager: ScreenManager, id: String) : Screen(manager) {
-    private val clientSong: Song? by mutableStateOf(app.mp.library[id]?.let { musicInfo ->
+class ScreenMusicDetails(manager: ScreenManager, private val id: String) : Screen(manager) {
+    private var clientSong: Song? by mutableStateOf(updateClientSong())
+    private var remoteSong: Song? by mutableStateOf(null)
+
+    private val pageComments = object : Pagination<SongComment, Long, Long>(
+        default = 0L,
+        pageNum = APIConfig.MIN_PAGE_NUM
+    ) {
+        override fun distinctValue(item: SongComment): Long = item.cid
+        override fun offset(item: SongComment): Long = item.cid
+    }
+
+    override val title: String by derivedStateOf { clientSong?.name ?: remoteSong?.name ?: "未知歌曲" }
+
+    private fun updateClientSong(): Song? = app.mp.library[id]?.let { musicInfo ->
         Song(
             id = musicInfo.id,
             version = musicInfo.version,
@@ -18,18 +48,54 @@ class ScreenMusicDetails(manager: ScreenManager, id: String) : Screen(manager) {
             lyricist = musicInfo.lyricist,
             composer = musicInfo.composer,
             album = musicInfo.album,
+            animation = musicInfo.path(Paths.modPath, ModResourceType.Animation).exists,
+            video = musicInfo.path(Paths.modPath, ModResourceType.Video).exists,
+            rhyme = musicInfo.path(Paths.modPath, ModResourceType.Rhyme).exists
         )
-    })
-    private val remoteSong: Song? by mutableStateOf(null)
-
-    override val title: String by derivedStateOf { clientSong?.name ?: remoteSong?.name ?: "未知歌曲" }
+    }
 
     override suspend fun initialize() {
 
     }
 
     @Composable
-    override fun Content(device: Device) {
+    private fun DetailsLayout() {
 
+    }
+
+    @Composable
+    private fun ResourceLayout() {
+
+    }
+
+    @Composable
+    override fun ActionScope.RightActions() {
+
+    }
+
+    @Composable
+    override fun BottomBar() {
+        if (app.config.userProfile != null) {
+
+        }
+    }
+
+    @Composable
+    private fun Portrait() {
+        val listState = rememberLazyListState()
+        PauseLoading(listState)
+    }
+
+    @Composable
+    private fun Landscape() {
+
+    }
+
+    @Composable
+    override fun Content(device: Device) {
+        when (device.type) {
+            Device.Type.PORTRAIT, Device.Type.SQUARE -> Portrait()
+            Device.Type.LANDSCAPE -> Landscape()
+        }
     }
 }
--- ylcs-app/cs/src/commonMain/kotlin/love/yinlin/data/rachel/song/Song.kt
+++ ylcs-app/cs/src/commonMain/kotlin/love/yinlin/data/rachel/song/Song.kt
@@ -2,22 +2,18 @@ package love.yinlin.data.rachel.song
 
 import androidx.compose.runtime.Stable
 import kotlinx.serialization.Serializable
-import love.yinlin.Local
-import love.yinlin.api.ServerRes
 
 @Stable
 @Serializable
 data class Song(
-    val sid: Int, // [歌曲编号]
     val id: String,
-    val version: String, // [版本]
-    val name: String, // [曲名]
-    val singer: String, //[歌手名]
-    val lyricist: String, // [词作]
-    val composer: String, // [作曲人]
-    val album: String, // [专辑]
-    val bgd: Boolean, // [是否有伴奏]
-    val video: Boolean, // [是否有MV]
-) {
-    val recordPath: String by lazy { "${Local.API_BASE_URL}/${ServerRes.Song.song(sid)}" }
-}
+    val version: String,
+    val name: String,
+    val singer: String,
+    val lyricist: String,
+    val composer: String,
+    val album: String,
+    val animation: Boolean = false,
+    val video: Boolean = false,
+    val rhyme: Boolean = false,
+)
