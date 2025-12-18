dbe5900adb3b09c8147ad59b419b1ec7016eb5d8
masterQian <76944654+masterQian@users.noreply.github.com>
2025-12-18 12:50:56 +0800

    style: 提出jvm参数

--- ylcs-app/desktopApp/build.gradle.kts
+++ ylcs-app/desktopApp/build.gradle.kts
@@ -26,19 +26,21 @@ kotlin {
     }
 }
 
+val desktopJvmArgs = buildList {
+    if ("desktopRun" in currentTaskName) {
+        val desktopWorkSpace = C.root.work.desktop.asFile
+        desktopWorkSpace.mkdirs()
+        add("-Duser.dir=$desktopWorkSpace")
+        add("-Djava.library.path=${C.root.native.libs}")
+    }
+    add("--enable-native-access=ALL-UNNAMED")
+}
+
 compose.desktop {
     application {
         mainClass = C.app.mainClass
 
-        if ("desktopRun" in currentTaskName) {
-            val desktopWorkSpace = C.root.work.desktop.asFile
-            desktopWorkSpace.mkdirs()
-            jvmArgs += arrayOf(
-                "-Duser.dir=$desktopWorkSpace",
-                "-Djava.library.path=${C.root.native.libs}",
-            )
-        }
-        jvmArgs += "--enable-native-access=ALL-UNNAMED"
+        jvmArgs += desktopJvmArgs
 
         buildTypes.release.proguard {
             version = C.proguard.version
