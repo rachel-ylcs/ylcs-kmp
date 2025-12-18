8dbbe0a6c6c3370fb5ed938055f7d6a0769c1b9f
masterQian <76944654+masterQian@users.noreply.github.com>
2025-12-18 15:36:04 +0800

    Update Context.desktop.kt

--- ylcs-module/compose/context/src/desktopMain/kotlin/love/yinlin/Context.desktop.kt
+++ ylcs-module/compose/context/src/desktopMain/kotlin/love/yinlin/Context.desktop.kt
@@ -7,7 +7,6 @@ import androidx.compose.ui.awt.ComposeWindow
 actual class Context actual constructor(delegate: PlatformContextDelegate) {
     // 在 initDelay 后可用
     lateinit var window: ComposeWindow
-    // 在 initDelay 后可用
     val handle: Long get() = window.windowHandle
 
     fun bindWindow(window: ComposeWindow) {
