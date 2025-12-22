daebb2c70eca64a22c28c4ff33b4b692e09609aa
masterQian <76944654+masterQian@users.noreply.github.com>
2025-12-22 19:41:40 +0800

    perf: 优化js的ArrayBufferSource

--- ylcs-base/core/src/wasmJsMain/kotlin/love/yinlin/io/ArrayBufferSource.kt
+++ ylcs-base/core/src/wasmJsMain/kotlin/love/yinlin/io/ArrayBufferSource.kt
@@ -7,18 +7,19 @@ import org.khronos.webgl.Int8Array
 import org.khronos.webgl.toByteArray
 
 class ArrayBufferSource(buffer: ArrayBuffer): RawSource {
-    private var position = 0L
-    private val bytes = Int8Array(buffer).toByteArray()
+    private var position = 0
+    private val bufferArray = Int8Array(buffer)
 
     override fun readAtMostTo(sink: Buffer, byteCount: Long): Long {
         if (byteCount == 0L) return 0L
         require(byteCount >= 0L) { "byteCount $byteCount < 0" }
-        val endPos = minOf(position + byteCount, bytes.size.toLong())
+        val endPos = minOf((position + byteCount).toInt(), bufferArray.length)
         val readTotal = endPos - position
-        if (readTotal == 0L) return -1L
-        sink.write(bytes, position.toInt(), endPos.toInt())
+        if (readTotal == 0) return -1L
+        val data = bufferArray.subarray(position, endPos).toByteArray()
+        sink.write(data, 0, readTotal)
         position = endPos
-        return readTotal
+        return readTotal.toLong()
     }
 
     override fun close() {}
