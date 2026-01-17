28e1d878135e6d5a382f30d715821a2d3b2787f3
masterQian <76944654+masterQian@users.noreply.github.com>
2026-01-17 13:12:32 +0800

    feat: native模块可选编译支持

--- buildSrc/src/main/kotlin/KotlinJvmTemplate.kt
+++ buildSrc/src/main/kotlin/KotlinJvmTemplate.kt
@@ -52,11 +52,14 @@ abstract class KotlinJvmTemplate : KotlinTemplate<KotlinJvmExtension>() {
             // 检查是否需要编译 Desktop Native
             val sourceDir = layout.projectDirectory.asFile.resolve("src/main/cpp")
             if (sourceDir.exists()) {
-                val buildNativeTask = tasks.register("buildDesktopNative", BuildDesktopNativeTask::class) {
-                    inputDir.set(sourceDir)
-                }
-                tasks.named("jar") {
-                    dependsOn(buildNativeTask)
+                // 检查是否忽略编译
+                if (!sourceDir.resolve("build.bat").exists() && !sourceDir.resolve("build.sh").exists()) {
+                    val buildNativeTask = tasks.register("buildDesktopNative", BuildDesktopNativeTask::class) {
+                        inputDir.set(sourceDir)
+                    }
+                    tasks.named("jar") {
+                        dependsOn(buildNativeTask)
+                    }
                 }
             }
         }
