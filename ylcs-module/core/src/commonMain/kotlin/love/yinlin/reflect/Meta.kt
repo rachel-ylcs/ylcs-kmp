a4d095bbd7ace1665619bd9430f7e5db905ce5b7
masterQian <76944654+masterQian@users.noreply.github.com>
2026-01-20 12:16:01 +0800

    fix: meta jvmname conflict

--- ylcs-module/core/src/commonMain/kotlin/love/yinlin/reflect/Meta.kt
+++ ylcs-module/core/src/commonMain/kotlin/love/yinlin/reflect/Meta.kt
@@ -1,11 +1,13 @@
 package love.yinlin.reflect
 
 import love.yinlin.annotation.CompatibleRachelApi
+import kotlin.jvm.JvmName
 
 inline fun <reified T> metaSimpleClassName(): String = T::class.simpleName!!
 
 val <T : Any> T.metaSimpleClassName: String get() = this::class.simpleName!!
 
+@get:JvmName("metaSimpleClassNameByType")
 inline val <reified T> T.metaSimpleClassName: String get() = T::class.simpleName!!
 
 @CompatibleRachelApi
@@ -15,6 +17,7 @@ expect inline fun <reified T> metaClassName(): String
 expect val <T : Any> T.metaClassName: String
 
 @CompatibleRachelApi
+@get:JvmName("metaClassNameByType")
 inline val <reified T> T.metaClassName: String get() = metaClassName<T>()
 
 @CompatibleRachelApi
@@ -24,4 +27,5 @@ expect inline fun <reified T> metaIsAnonymousClass(): Boolean
 expect val <T : Any> T.metaIsAnonymousClass: Boolean
 
 @CompatibleRachelApi
+@get:JvmName("metaIsAnonymousClassByType")
 inline val <reified T> T.metaIsAnonymousClass: Boolean get() = metaIsAnonymousClass<T>()
