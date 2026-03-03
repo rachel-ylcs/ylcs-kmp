f459024084fb69ace34b023ccfc20c4fed8e1ec3
masterQian <76944654+masterQian@users.noreply.github.com>
2026-03-03 20:22:38 +0800

    fix: 修复AGP嵌入旧版dokka输出javadoc失败的问题

--- ylcs-module/compose/plugin/animated-webp-decoder-ndk/build.gradle.kts
+++ ylcs-module/compose/plugin/animated-webp-decoder-ndk/build.gradle.kts
@@ -6,4 +6,7 @@ plugins {
     )
 }
 
+// https://github.com/Kotlin/dokka/issues/2956
+tasks.matching { it.name.contains("javaDocReleaseGeneration", ignoreCase = true) }.configureEach { enabled = false }
+
 template(object : KotlinAndroidNDKTemplate() {})
