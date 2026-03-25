6377546ede6ea050e7eed8eccfe00bb4777578e7
masterQian <76944654+masterQian@users.noreply.github.com>
2026-03-25 14:36:21 +0800

    update: 3.5.5

--- ylcs-app/app/account/src/commonMain/kotlin/love/yinlin/data/rachel/literal/AppAbout.kt
+++ ylcs-app/app/account/src/commonMain/kotlin/love/yinlin/data/rachel/literal/AppAbout.kt
@@ -9,10 +9,10 @@ import love.yinlin.compose.ui.icon.Icons
 internal object AppAbout {
      val updateInfo = AppUpdateInfo(
         platform = "Android/iOS/Windows/Linux/macOS/Web 发布",
-        title = "银临茶舍 歌词编辑器上线",
+        title = "银临茶舍 音乐播放器更新",
         force = false,
         maintenance = false,
-        date = LocalDate(2026, 3, 17),
+        date = LocalDate(2026, 3, 25),
         groups = listOf(
             AppUpdateRecordGroup(
                 type = "特性",
@@ -23,7 +23,7 @@ internal object AppAbout {
                     "1. 伴奏系统上线, 全曲库纯净伴奏解锁",
                     "2. 动态歌词引擎支持, 与音游共享配置",
                     "3. 官网落地, 支持在线下载和查看使用教程",
-                    "4. 音游内测上线, 内测期将陆续支持银临全曲集, 公测后将开放玩家排行榜",
+                    "4. 音游内测上线, 内测期将陆续支持银临全曲集",
                 )
             ),
             AppUpdateRecordGroup(
@@ -32,14 +32,14 @@ internal object AppAbout {
                 color = Colors.Pink4,
                 background = Colors.Pink2,
                 records = listOf(
-                    "1. MOD歌词编辑器支持",
-                    "2. 曲库和工坊支持筛选搜索",
-                    "3. 下拉刷新和下拉加载列表启用悬浮球设计, 移除部分FAB",
-                    "4. 电脑端横向滚动鼠标滑轮适配",
-                    "5. 安卓端悬浮歌词顶部偏移改为比例计算",
-                    "6. 增加部分专辑音游配置",
-                    "7. 减小安装包体积",
-                    "8. 优化安卓端歌词渲染引擎性能",
+                    "1. 优化播放器、歌单、曲库等UI界面的展示",
+                    "2. 歌单顺序随时调整，不需要停止播放器",
+                    "3. 安卓端主页返回可以退到桌面",
+                    "4. 优化选项弹窗的最小高度",
+                    "5. 悬浮歌词增加背景色设置、重置恢复默认设置",
+                    "6. 优化密码输入显示按钮长按式为点击切换式",
+                    "7. 简化部分页面，无标题栏风格",
+                    "8. 减小安装包体积, 优化性能",
                 )
             ),
             AppUpdateRecordGroup(
@@ -48,11 +48,13 @@ internal object AppAbout {
                 color = Colors.Red4,
                 background = Colors.Red2,
                 records = listOf(
-                    "1. 修复音乐页未初始化的问题",
-                    "2. 修复表情显示崩溃的问题",
-                    "3. 修复游戏页异常提示的问题",
-                    "4. 修复安卓端悬浮歌词关闭后无法再打开的问题",
-                    "5. 修复主题帖链接不显示的问题",
+                    "1. 修复空白处无法上拉加载的问题",
+                    "2. 修复安卓端悬浮歌词遮挡屏幕的问题",
+                    "3. 修复无法进入抖音板块的问题",
+                    "4. 修复图表大小异常的问题",
+                    "5. 修复歌词编辑器崩溃的问题",
+                    "6. 修复平板设置页不能滚动的问题",
+                    "7. 修复电脑端悬浮歌词设置不生效的问题",
                 )
             )
         )
