4619a8c9357b69bace2bb5e03636e395a14c1e7b
masterQian <76944654+masterQian@users.noreply.github.com>
2026-08-29 22:01:01 +0800

    fix: 还原lifecycle的上游修复

--- ylcs-module/compose/screen/src/commonMain/kotlin/love/yinlin/compose/screen/ScreenManager.kt
+++ ylcs-module/compose/screen/src/commonMain/kotlin/love/yinlin/compose/screen/ScreenManager.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
 import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
 import androidx.navigation3.ui.NavDisplay
@@ -25,7 +26,6 @@ import love.yinlin.extension.Array
 import love.yinlin.extension.DateEx
 import love.yinlin.extension.cast
 import love.yinlin.extension.parseJson
-import love.yinlin.extension.then
 
 @Stable
 class ScreenManager @PublishedApi internal constructor(savedBackStack: List<String>) {
@@ -72,7 +72,7 @@ class ScreenManager @PublishedApi internal constructor(savedBackStack: List<Stri
                     popTransitionSpec = transitionSpecProvider,
                     entryDecorators = listOf(
                         rememberSaveableStateHolderNavEntryDecorator(),
-                        rememberCompatibleViewModelStoreNavEntryDecorator(),
+                        rememberViewModelStoreNavEntryDecorator(),
                     ),
                     entryProvider = { manager.registerScreen(map, it) }
                 )
