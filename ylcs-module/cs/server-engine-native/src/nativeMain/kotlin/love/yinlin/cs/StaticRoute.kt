99878e2a8679209a0b56f7b27f75fa8ac1b6f76f
masterQian <76944654+masterQian@users.noreply.github.com>
2026-09-03 20:15:33 +0800

    feat: 静态路由文件支持

--- ylcs-module/cs/server-engine-native/src/nativeMain/kotlin/love/yinlin/cs/StaticRoute.kt
+++ ylcs-module/cs/server-engine-native/src/nativeMain/kotlin/love/yinlin/cs/StaticRoute.kt
@@ -1,25 +1,11 @@
 package love.yinlin.cs
 
-import io.ktor.http.ContentType
-import io.ktor.http.charset
-import io.ktor.http.content.OutgoingContent
-import io.ktor.http.fromFileExtension
-import io.ktor.http.withCharset
-import io.ktor.server.application.ApplicationCall
+import io.ktor.http.*
 import io.ktor.server.response.respond
-import io.ktor.server.routing.Route
-import io.ktor.server.routing.RouteSelector
-import io.ktor.server.routing.RouteSelectorEvaluation
-import io.ktor.server.routing.RoutingResolveContext
-import io.ktor.server.routing.get
-import io.ktor.server.routing.route
+import io.ktor.server.response.respondSource
+import io.ktor.server.routing.*
 import io.ktor.util.AttributeKey
-import io.ktor.utils.io.ByteReadChannel
 import io.ktor.utils.io.charsets.Charsets
-import kotlinx.io.Buffer
-import kotlinx.io.IOException
-import kotlinx.io.RawSource
-import kotlinx.io.buffered
 import love.yinlin.fs.File
 import love.yinlin.fs.PlatformFileSystem
 import kotlin.collections.firstOrNull
@@ -30,8 +16,6 @@ private object TailcardSelector : RouteSelector() {
 
     override fun toString(): String = "(static-content)"
 }
-
-private val StaticFileLocationProperty: AttributeKey<String> = AttributeKey("StaticFileLocation")
 
 private const val pathParameterName = "static-content-path-parameter"
 
@@ -46,57 +30,31 @@ private fun ContentType.matchApplicationTypeWithCharset(): Boolean = match(Conte
     else -> false
 }
 
-private class BoundedRawSource(private val delegate: RawSource, private var remaining: Long) : RawSource {
-    override fun readAtMostTo(sink: Buffer, byteCount: Long): Long {
-        require(byteCount >= 0L) { "byteCount must be >= 0, but was $byteCount" }
-        if (remaining <= 0L) return -1L
-        val toRead = minOf(byteCount, remaining)
-        val bytesRead = delegate.readAtMostTo(sink, toRead)
-        if (bytesRead > 0L) remaining -= bytesRead
-        return bytesRead
-    }
-    override fun close() = delegate.close()
-}
-
-private class LocalFileContent(val file: File, override val contentType: ContentType) : OutgoingContent.ReadChannelContent() {
-    override val contentLength: Long get() = file.fileSizeSync()
-
-    init {
-        if (!file.existsSync()) throw IOException("No such file ${file.path}")
-    }
-
-    override fun readFrom(): ByteReadChannel = ByteReadChannel(file.bufferedSourceSync())
-
-    override fun readFrom(range: LongRange): ByteReadChannel {
-        val source = file.bufferedSourceSync()
-        if (range.first > 0) source.skip(range.first)
-        val length = range.last - range.first + 1
-        return ByteReadChannel(BoundedRawSource(source, length).buffered())
-    }
-}
-
-private suspend fun respondStaticFile(call: ApplicationCall, requestedFile: File) {
-    call.attributes.put(StaticFileLocationProperty, requestedFile.path)
-
-    val contentTypeList = ContentType.fromFileExtension(requestedFile.extension)
-    val type = contentTypeList.firstOrNull() ?: ContentType.Application.OctetStream
-    val contentType = when {
-        type.match(ContentType.Text.Any) -> type.withCharsetUTF8IfNeeded()
-        type.match(ContentType.Image.SVG) -> type.withCharsetUTF8IfNeeded()
-        type.matchApplicationTypeWithCharset() -> type.withCharsetUTF8IfNeeded()
-        else -> type
-    }
-
-    if (requestedFile.isFile()) call.respond(LocalFileContent(requestedFile, contentType))
-}
-
 internal fun Route.staticFiles(remotePath: String, dir: File): Route = createChild(TailcardSelector).route(remotePath) {
     route("{${pathParameterName}...}") {
         get {
-            val relativePath = call.parameters.getAll(pathParameterName)?.joinToString(PlatformFileSystem.PathSeparator.toString()) ?: return@get
-            val requestedFile = File(dir, relativePath)
-            val actualFile = if (requestedFile.isDirectory()) File(requestedFile, "index.html") else requestedFile
-            respondStaticFile(call, actualFile)
+            val relativePath = call.parameters.getAll(pathParameterName)?.joinToString(PlatformFileSystem.PathSeparator.toString())
+            if (relativePath != null) {
+                val file = File(dir, relativePath)
+                val requestedFile = if (file.isDirectory()) File(file, "index.html") else file
+                if (requestedFile.exists()) {
+                    val contentTypeList = ContentType.fromFileExtension(requestedFile.extension)
+                    val type = contentTypeList.firstOrNull() ?: ContentType.Application.OctetStream
+                    val contentType = when {
+                        type.match(ContentType.Text.Any) -> type.withCharsetUTF8IfNeeded()
+                        type.match(ContentType.Image.SVG) -> type.withCharsetUTF8IfNeeded()
+                        type.matchApplicationTypeWithCharset() -> type.withCharsetUTF8IfNeeded()
+                        else -> type
+                    }
+
+                    call.attributes.put(AttributeKey("StaticFileLocation"), requestedFile.path)
+                    requestedFile.rawSource().use { source ->
+                        call.respondSource(source, contentType, contentLength = requestedFile.fileSize())
+                    }
+                    return@get
+                }
+            }
+            call.respond(HttpStatusCode.NotFound)
         }
     }
 }
