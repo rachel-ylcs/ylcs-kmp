58933c5cb2f004fc58742f6d764c0ab44c8acae3
masterQian <76944654+masterQian@users.noreply.github.com>
2026-09-24 00:03:14 +0800

    fix: 去除Layer.kt警告

--- ylcs-module/compose/game/core/src/commonMain/kotlin/love/yinlin/compose/game/traits/Layer.kt
+++ ylcs-module/compose/game/core/src/commonMain/kotlin/love/yinlin/compose/game/traits/Layer.kt
@@ -140,6 +140,7 @@ open class Layer(
         return null
     }
 
+    @Suppress("REDUNDANT_ELSE_IN_WHEN")
     internal fun triggerVisibleLayer(event: Event): Boolean = when (event) {
         // 检查是否是指针事件 拦截
         is Event.Pointer -> {
